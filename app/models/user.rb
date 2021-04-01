8f41139772918484849c24000bca5bc65f7bd28a
Andrew <andrew.ku93@gmail.com>
2021-04-01 22:16:27 +0000

    add new user form and validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
+  validates :email, uniqueness: true
 end
