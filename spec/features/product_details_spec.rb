1569384427cd2a0367c8c9420f5326169fffd7e9
Andrew <andrew.ku93@gmail.com>
2021-04-06 18:36:51 +0000

    add product_detail_spec clicking on 'details' btn/link to display the product page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.feature "Visits a product page", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    2.times do
+      @category.products.create!(
+        name: Faker::Hipster.sentence(2),
+        description: Faker::Hipster.paragraph(6),
+        image: open_asset("apparel1.jpg"),
+        quantity: 20,
+        price: 49.99
+      )
+    end
+  end
+  
+  scenario "Sees the details of the product" do
+    visit root_path
+    click_link("Details", match: :first)
+
+    expect(page).to have_css("article.product-detail")
+    
+    save_screenshot "Test2_product_page.png" # Screenshot is slow
+  end
+
+end
